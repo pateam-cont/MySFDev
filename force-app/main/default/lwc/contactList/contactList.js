0d803ceb097b3a164ed2bf12a919afab50dd8323
Amita <patelamita81@gmail.com>
2021-08-26 11:31:05 +0530

    LWC super badge

---
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,21 @@
+import { LightningElement,api,track, wire} from 'lwc';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+import CONTACT_FIRSTNAME from '@salesforce/schema/Contact.FirstName';
+import CONTACT_LASTNAME from '@salesforce/schema/Contact.LastName';
+import CONTACT_EMAIL from '@salesforce/schema/Contact.Email';
+import { reduceErrors } from 'c/ldsUtils';
+const COLUMNS = [
+    { label: 'First Name', fieldName: CONTACT_FIRSTNAME.fieldApiName, type: 'text' },
+    { label: 'Last Name', fieldName: CONTACT_LASTNAME.fieldApiName, type: 'text' },
+    { label: 'Email', fieldName: CONTACT_EMAIL.fieldApiName, type: 'text' }
+];
+export default class ContactList extends LightningElement {
+    columns=COLUMNS
+
+    @wire(getContacts)
+    contacts;
+    get errors() {
+        return (this.contacts.error) ?
+            reduceErrors(this.contacts.error) : [];
+    }
+}
